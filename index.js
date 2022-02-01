4b41f72842ddb1057b9b1fb8833dcce486552de1
Ibaihima <ibaihima@hotmail.com>
2022-02-01 01:10:58 -0500

    commit tree bd1215c31ac9bb2a3108d29f521e49dc36cb3bca

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+function saturdayFun(word ="roller-skate"){
+    return (`This Saturday, I want to ${word}!`);
+} 
+function mondayWork(newWord ="go to the office"){
+    return (`This Monday, I will ${newWord}.`)
+}
+let wrapAdjective = function(wrap="*") {
+     return function (adjective="special") {
+        return `You are ${wrap}${adjective}${wrap}!`
+     }
+}
